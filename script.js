08d84b71280ff8db0eded5c56ab8109762051ddb
anilcodersigdel <anilsigdeel594@gmail.com>
2024-05-14 07:13:29 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburgerMenu = document.getElementById('hamburger-menu');
+    // const navList = document.querySelector('.navbar ul');
+    const navList = document.querySelector('nav ul');
+
+    hamburgerMenu.addEventListener('click', () => {
+        // navList.classList.toggle('show');
+        navList.classList.toggle('show');
+    });
